d00c5e9661b217496711b0570f8acc66f9d9ff69
tgarrels <tgarrels@garrels-acer-swift>
2020-03-19 13:51:53 +0100

    Removed update (dev)command

--- bot.py
+++ bot.py
@@ -20,11 +20,6 @@ def main():
     start_handler = CommandHandler('start', telegram_interaction.start_command_callback)
     dispatcher.add_handler(start_handler)
 
-    # TODO: For dev purpose only
-    # Register /update command handler
-    update_handler = CommandHandler('update', update)
-    dispatcher.add_handler(update_handler)
-
     # Register /describe command handler
     describe_handler = CommandHandler('describe', telegram_interaction.send_current_description)
     dispatcher.add_handler(describe_handler)
@@ -36,27 +31,6 @@ def main():
     # Start Bot
     updater.start_polling()
     updater.idle()
-
-def update(update, context):
-    """Hook to redeploy prod bot via github webhook
-    Performs a git pull and a restart of bot"""
-    FNULL = open(os.devnull, 'w')
-    try:
-        # Make sure server is up to date
-        subprocess.Popen(['git', 'pull'], stdout=FNULL)
-        # Make sure there are no local changes on server
-        subprocess.Popen(['git', 'reset', '--hard'], stdout=FNULL)
-        # Log the pull
-        with open('logs/last_pull', 'w+') as pull_log:
-            pull_log.write(str(datetime.now()))
-        # Restart the server
-        subprocess.Popen(['./manage.sh', 'restart'], stdout=FNULL)
-    except Exception as exception:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Something went wrong!")
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=str(exception))
-
 
 if __name__ == "__main__":
     main()
